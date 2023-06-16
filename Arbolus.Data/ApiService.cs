54d12f9fd2b042abc4a39b185b8c3f2527a3e08f
pradeep_kumar3325 <pradeep_kumar3325@yahoo.in>
2023-06-16 17:57:46 +0530

    Temp Changes

---
+++ Arbolus.Data/ApiService.cs
@@ -0,0 +1,24 @@
+﻿using Arbolus.Model.Interface;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Net.Http;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arbolus.Data
+{
+    public class ApiService : IApiService
+    {
+        private HttpClient httpClient;
+
+        public string SvcUrl { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+        public int RequestUri { get => throw new NotImplementedException(); set => throw new NotImplementedException(); }
+
+        public Task<string> Get()
+        {
+            throw new NotImplementedException();
+        }
+        
+    }
+}

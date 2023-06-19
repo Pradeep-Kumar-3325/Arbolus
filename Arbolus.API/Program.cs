45a04ea6e6a01e423ed1bd53d196c77eaeb8bf43
pradeep_kumar3325 <pradeep_kumar3325@yahoo.in>
2023-06-19 10:44:27 +0530

    Error

--- Arbolus.API/Program.cs
+++ Arbolus.API/Program.cs
@@ -20,7 +20,9 @@ namespace Arbolus.API
             Host.CreateDefaultBuilder(args)
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
-                    webBuilder.UseStartup<Startup>();
+                    webBuilder.UseStartup<Startup>()
+                    .CaptureStartupErrors(true);
+                    
                 });
     }
 }
